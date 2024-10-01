99ad1019bfb1dce59b7482e75ddbec082942c299
Irfan <103058558+mirfankazi@users.noreply.github.com>
2024-09-30 21:06:48 -0400

    Update and rename b2.php to hero_banner_image.php

---
+++ playgrounds/react/components/hero_banner_image.php
@@ -0,0 +1,26 @@
+<section class="section section-light-down section-top section-trim-bottom">
+    <div class="container">
+
+        <div class="row row-cols-1 row-cols-lg-2">
+            <div class="col">
+
+                <header class="section-header section-header-lg">
+                    <div class="section-header-eyebrow">About Us</div>
+                    <h1 class="section-header-title">Hero Banner [Image]</h1>
+                    <p class="section-header-subtitle mb-4 mb-md-6">Imperdiet varius turpis a non nullam laoreet ac in tellus</p>
+
+                    <div class="gap gap-tight">
+                        <a class="btn btn-primary" href="#">Contact us</a>
+                        <a class="btn btn-outline-primary" href="#">Secondary CTA</a>
+                    </div>
+                </header>
+
+            </div>
+            <div class="col align-self-end text-center d-none d-lg-block">
+
+                <img class="img-fluid" src="https://assets.ot.digital/img/webp/example640x468.webp" width="640" height="468" alt="">
+            </div>
+        </div>
+
+    </div>
+</section>
