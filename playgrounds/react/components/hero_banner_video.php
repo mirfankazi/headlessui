3c90e2a7c3933888787de5e0bc72ed669ee0a9d0
Irfan <103058558+mirfankazi@users.noreply.github.com>
2024-09-30 21:07:50 -0400

    Update and rename b3 to hero_banner_video.php

---
+++ playgrounds/react/components/hero_banner_video.php
@@ -0,0 +1,47 @@
+<section class="section section-light-down section-top section-trim-bottom">
+    <div class="container">
+        
+        <div class="row row-cols-1 row-cols-lg-2">
+            <div class="col">
+                
+                <header class="section-header section-header-lg">
+                    <div class="section-header-eyebrow">About Us</div>
+                    <h1 class="section-header-title">About OpenText</h1>
+                    <p class="section-header-subtitle mb-4 mb-md-6">Imperdiet varius turpis a non nullam laoreet ac in tellus</p>
+
+                    <div class="gap gap-tight">
+                      <a class="btn btn-primary" href="#">Contact us</a>
+                      <a class="btn btn-outline-primary" href="#">Secondary CTA</a>
+                    </div>
+                </header>
+                
+            </div>
+            <div class="col align-self-end">
+
+                <figure class="mb-8">
+                    <div class="vidyard-player-wrap">
+                        <img
+                            class="figure-img img-fluid vidyard-player-embed js-embed-vidyard"
+                            src="https://play.vidyard.com/BLCrQbmEvW3iwidvmSEWWQ.jpg"
+                            data-uuid="BLCrQbmEvW3iwidvmSEWWQ"
+                            data-v="4"
+                            data-type="inline"
+                            alt="">
+                            
+                        <div class="vidyard-play">
+                            <div class="vidyard-play-bg"></div>
+                            <svg class="vidyard-play-svg" viewBox="0 0 32 32" fill="#fff" focusable="false" role="img" aria-label="Play video">
+                                <title>Play video</title>
+                                <path d="M6.484 4.094l20.75 11.225c0.226 0.121 0.41 0.427 0.41 0.683s-0.184 0.563-0.41 0.683l-20.75 11.222c-0.095 0.051-0.26 0.093-0.367 0.093-0.427 0-0.774-0.346-0.774-0.773v-22.451c0-0.428 0.347-0.774 0.774-0.774 0.108 0 0.272 0.042 0.367 0.093z"></path>
+                            </svg>
+                        </div>
+                    </div>
+                    
+                    <figcaption class="figure-caption text-center">An optional descriptive caption about this video.</figcaption>
+                </figure>
+                
+            </div>
+        </div>
+        
+    </div>
+</section>
